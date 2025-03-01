9a79a668b57b2788f287ce4897b5e9a1b8ba0fb5
wognsths <2021122006@yonsei.ac.kr>
2025-03-01 13:07:03 +0900

    structure

---
+++ Data/Crawler/NaverNews.py
@@ -0,0 +1,9 @@
+from Data.Crawler.utils.logger import setup_logger
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import pandas as pd
+
+import time
+import os
